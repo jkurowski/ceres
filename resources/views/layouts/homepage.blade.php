3e208d2040dd486e57aeeb726e95c39a4bab7f2e
Jacek <kontakt@jk-design.pl>
2026-07-20 08:52:14 +0200

    [Jacek][Fix] Karta mieszkania

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -20,11 +20,11 @@
 
     <!-- CSS (non-blocking) -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" media="print" onload="this.media='all'">
-    <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}?v=20072026A" media="print" onload="this.media='all'">
+    <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}?v=20072026E" media="print" onload="this.media='all'">
 
     <!-- fallback dla no-js -->
     <noscript>
-        <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}?v=20072026A">
+        <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}?v=20072026E">
     </noscript>
 
     <!-- Preloads -->
